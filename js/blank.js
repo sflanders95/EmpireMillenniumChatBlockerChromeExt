66e047d0ae23900cf5af34163f3928ab66afdd61
Steven Flanders <Steve.flanders@live.com>
2019-01-20 11:14:05 -0500

    .

--- js/blank.js
+++ js/blank.js
@@ -13,10 +13,10 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function displayChat(sAl, sGl, timeStamp) {
-  // var sHtml = '<html><head><meta charset="utf-8">';
-  // sHtml += '<link href="https://game.empiremillenniumwars.com/game.css?vLive.1.35.0" rel="stylesheet">';
-  // sHtml += '<style>body {background-color:rgba(0,0,39,.75);color:#d8d8d8;}.button--size-fill{height:unset;}</style>';
-  // sHtml += '</head><body>';
+  /* var sHtml = '<html><head><meta charset="utf-8">';
+     sHtml += '<link href="https://game.empiremillenniumwars.com/game.css?vLive.1.35.0" rel="stylesheet">';
+     sHtml += '<style>body {background-color:rgba(0,0,39,.75);color:#d8d8d8;}.button--size-fill{height:unset;}</style>';
+     sHtml += '</head><body>';  */
   var sHtml = '<table><tr><td colspan="2">' + timeStamp;
   sHtml += '</td></tr><tr><td>Global</td><td>Alliance</td></tr><tr><td>';
   sHtml += sGl + '</td><td>' + sAl;
